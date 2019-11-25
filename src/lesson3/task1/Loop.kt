3dbb77f6427c40494b9bd5264e8cb03a6b82647d
kok_magic <spikebrony@mail.ru>
2019-11-25 22:41:07 +0300

    четвертый урок

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -156,9 +156,7 @@ fun maxDivisor(n: Int): Int = n / minDivisor(n)
  * Взаимно простые числа не имеют общих делителей, кроме 1.
  * Например, 25 и 49 взаимно простые, а 6 и 8 -- нет.
  */
-fun isCoPrime(m: Int, n: Int): Boolean {
-    return lcm(m, n) == m * n
-}
+fun isCoPrime(m: Int, n: Int): Boolean = lcm(m, n) == m * n
 
 
 /**
